73d03fd69b814dab05228d2ca2f9ad489b9e43b1
Matota <72810000+mat0ta@users.noreply.github.com>
2021-12-14 12:05:01 +0100

    Update torres.py

--- Ejercicio 2/torres.py
+++ Ejercicio 2/torres.py
@@ -7,6 +7,8 @@ class JuegoTorres():
         self.torres1 = 0
         self.torres2 = 0
         self.asignacion = 0
+        self.movimientos_1 = 0
+        self.movimientos_2 = 0
 
     def crearCuadros(self):
         for i in range(3):
@@ -38,3 +40,37 @@ class JuegoTorres():
                     y = random.randint(0, 2)
             self.asignacion = 2
             return
+    def jugador1(self):
+            for x in range(0, 3):
+                for y in range(0, 3):
+                    global moved
+                    moved = False
+                    print(x, y)
+                    if self.cuadros[x][y] == '♜':
+                        global xtemp
+                        xtemp = x
+                        xtemp = xtemp + 1
+                        if xtemp < 3 and xtemp > -1 and self.cuadros[xtemp][y] == ' ':
+                            self.cuadros[xtemp][y] = '♜'
+                            self.cuadros[x][y] = ' '
+                            print('Ficha movida de ' + str(x) + ',' +
+                                str(y) + ' a ' + str(xtemp) + ',' + str(y))
+                            self.movimientos_1 += 1
+                            j.printearCuadros()
+                            time.sleep(2)
+                            break
+                        else:
+                            xtemp = x - 1
+                            if xtemp < 3 and xtemp > -1 and self.cuadros[xtemp][y] == ' ':
+                                self.cuadros[xtemp][y] = '♜'
+                                self.cuadros[x][y] = ' '
+                                print('Ficha movida de ' + str(x) + ',' +
+                                    str(y) + ' a ' + str(xtemp) + ',' + str(y))
+                                self.movimientos_1 += 1
+                                j.printearTablero()
+                                time.sleep(2)
+                                break
+            return self.movimientos_1
+
+
+j = JuegoTorres()
