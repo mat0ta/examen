c27bb6aef6e4deaa0ff0f213e91a1dff4e3444a0
Matota <72810000+mat0ta@users.noreply.github.com>
2021-12-14 11:42:42 +0100

    Update torres.py

--- Ejercicio 2/torres.py
+++ Ejercicio 2/torres.py
@@ -1,16 +1,18 @@
 import random
 import time
 
-cuadros = []
-torres1 = 0
-torres2 = 0
-asignacion = 0
+class JuegoTorres():
+    def __init__(self):
+        self.cuadros = []
+        self.torres1 = 0
+        self.torres2 = 0
+        self.asignacion = 0
 
-def crearTablero():
-    for i in range(3):
-        cuadros.append(([' ']*3))
+    def crearTablero(self):
+        for i in range(3):
+            self.cuadros.append(([' ']*3))
 
 
-def printearTablero():
-    for j in range(3):
-        print(cuadros[j])
+    def printearTablero(self):
+        for j in range(3):
+            print(self.cuadros[j])
