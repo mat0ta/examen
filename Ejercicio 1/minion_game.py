7f82f1169de487c621903f7ba72e28d344395e06
Matota <72810000+mat0ta@users.noreply.github.com>
2021-12-14 11:54:29 +0100

    Finalizada función de Stuard

--- Ejercicio 1/minion_game.py
+++ Ejercicio 1/minion_game.py
@@ -17,17 +17,24 @@ class minion_game():
             return False
     def stuart(self):
         start = 0
+        num = 1
         while True:
-            num = int(random.randint(0, len(self.string_1)))
-            if not m.checkLetra(self.string[start:(num+1)]):
-                self.string_1 = self.string[start:(num+1)]
+            if not m.checkLetra(self.string[start:num]):
+                self.string_1 = self.string[start:num]
                 if self.string_1 not in self.strings_1:
-                    self.string_1 = self.string[start:(num+1)]
+                    self.string_1 = self.string[start:num]
                     self.strings_1.append(self.string_1)
-                    self.puntuacion_1 += self.string.count(self.string_1)
+                    if self.string_1 != '':
+                        self.puntuacion_1 += self.string.count(self.string_1)
                     print(self.string.count(self.string_1), self.string_1, self.puntuacion_1)
+            num += 1
             if self.string_1.upper() == self.string:
-                start += 2
+                start += 1
+                while m.checkLetra(self.string[start:]):
+                    start += 1
+                    print('Doner')
+                print('Done', start)
+                num = start
 
 
 m = minion_game()
--- Ejercicio 2/torres.py
+++ Ejercicio 2/torres.py
@@ -8,11 +8,33 @@ class JuegoTorres():
         self.torres2 = 0
         self.asignacion = 0
 
-    def crearTablero(self):
+    def crearCuadros(self):
         for i in range(3):
             self.cuadros.append(([' ']*3))
 
 
-    def printearTablero(self):
+    def printearCuadros(self):
         for j in range(3):
             print(self.cuadros[j])
+    
+    def crearFichas(self):
+           while True:
+            x = random.randint(0, 2)
+            y = random.randint(0, 2)
+            while fichas_1 < 3 and self.asignacion == 0:
+                if self.cuadros[x][y] == ' ':
+                    self.cuadros[x][y] = '♜'
+                    fichas_1 = fichas_1 + 1
+                elif self.cuadros[x][y] == '♜':
+                    x = random.randint(0, 2)
+                    y = random.randint(0, 2)
+            self.asignacion = 1
+            while fichas_2 < 3 and self.asignacion == 1:
+                if self.cuadros[x][y] == ' ':
+                    self.cuadros[x][y] = '♖'
+                    fichas_2 = fichas_2 + 1
+                elif self.cuadros[x][y] == '♖' or self.cuadros[x][y] == '♜':
+                    x = random.randint(0, 2)
+                    y = random.randint(0, 2)
+            self.asignacion = 2
+            return
